04084abf7381a2039279a3be8757de148f1a9861
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2022-01-06 14:24:41 -0500

    finished category routes

--- fantastic-umbrella-main/fantastic-umbrella-main/Develop/routes/api/category-routes.js
+++ fantastic-umbrella-main/fantastic-umbrella-main/Develop/routes/api/category-routes.js
@@ -67,7 +67,6 @@ router.post("/", (req, res) => {
 router.put("/:id", (req, res) => {
   // update a category by its `id` value
   Category.update(req.body, {
-    inidividualHooks: true,
     where: {
       id: req.params.id,
     },
@@ -87,6 +86,22 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbCategoryData) => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: "No category found with this id!" });
+        return;
+      }
+      res.status(dbCategoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
